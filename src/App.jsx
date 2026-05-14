041dfe6d873229142dde045aa70a7853d7534e29
Dhanyaja <dhanyaja2003@gmail.com>
2026-05-14 13:22:32 +0530

    Navigate imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import EventListing from './pages/EventListing'
 import FeedBids from './pages/FeedBids'
 import BuyDrinks from './pages/BuyDrinks'
